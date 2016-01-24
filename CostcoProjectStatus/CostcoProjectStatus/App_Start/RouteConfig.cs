af082c78c53428ca57e88174b30f2a79518876e0
Firoozeh <mehrikal@seattleu.edu>
2016-01-24 13:08:33 -0800

    Defining the RoutMap

--- CostcoProjectStatus/CostcoProjectStatus/App_Start/RouteConfig.cs
+++ CostcoProjectStatus/CostcoProjectStatus/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace CostcoProjectStatus
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "ProjectList",
+                "ProjectList/{Display}",
+                new { Controller="ProjectList", action="Display",id=""}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ CostcoProjectStatus/CostcoProjectStatus/Controllers/ProjectListController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataService;
+using Newtonsoft.Json;
+
+namespace CostcoProjectStatus.Controllers
+{
+    public class ProjectListController : Controller
+    {
+
+        private AccessService DataAccasess = new AccessService();
+            // GET: ProjectList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProjectList/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectList/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectList/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectList/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectList/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectList/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectList/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Display()
+        {
+            var ProjectNames = DataAccasess.GetAllProjectNamess();
+
+            string ProjectNameJson = JsonConvert.SerializeObject(ProjectNames);
+
+                return View(ProjectNameJson);
+        }
+    }
+}
