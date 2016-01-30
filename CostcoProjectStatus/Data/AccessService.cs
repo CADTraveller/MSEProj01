657871c5e1cd677991f7af719a3239ac52a1b959
craig colomb <craig@craigcad.us>
2016-01-30 11:39:05 -0800

    Revised RecordStatusUpdate to take a List<StatusUpdate>

--- CostcoProjectStatus/Data/AccessService.cs
+++ CostcoProjectStatus/Data/AccessService.cs
@@ -7,7 +7,7 @@ using StatusUpdatesModel;
 
 namespace DataService
 {
-    public class AccessService 
+    public class AccessService
     {
         private CostcoDevStatusEntities context;
         private const string ConnectionString = "Server=tcp:costcosu.database.windows.net,1433;Database=CostcoDevStatus;User ID=SUAdmin@costcosu;Password=39ffbJeo;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
@@ -45,94 +45,8 @@ namespace DataService
         }
         #endregion
 
-        private List<StatusUpdate> GetAllUpdatesForProjectPhase(string projectID, int phaseID)
-        {
-            List<StatusUpdate> updates = new List<StatusUpdate>();
-            updates = context.StatusUpdates.Where(p => p.ProjectID == projectID && p.PhaseID == phaseID).ToList();
-            return updates;
-        }
 
-        public void RecordStatusUpdate(ProjectUpdate projectUpdate)
-        {
-            List<StatusUpdate> NewUpdates = projectUpdate.Updates;
-
-            //__this information from the projectUpdate will also be written to each status update entry
-            DateTime currentDT = DateTime.Now;
-            string newUpdateID = projectUpdate.ProjectID;
-            int iUpdatePhaseID = NewUpdates[0].PhaseID;// projectUpdate.PhaseID;
-            int iVerticalID = projectUpdate.VerticalID;
-            int iNewSequenceNumber = 0;
-
-            //__check to see if Project exists, add it if not
-
-            //Project existingProjectEntry = context.Projects.First(p => p.ProjectID == newUpdateID);
-            Project existingProjectEntry = context.Projects.FirstOrDefault(p => p.ProjectID == newUpdateID);
-            if (existingProjectEntry == null) //_if there is no existing entry then add one
-            {
-                context.Projects.Add(new Project()
-                {
-                    ProjectID = newUpdateID,
-                    VerticalID = iVerticalID
-                });
-            }
-
-            ProjectPhase projectPhaseEntry = context.ProjectPhases.FirstOrDefault(p => p.ProjectID == newUpdateID && p.PhaseID == iUpdatePhaseID);
-
-            if (projectPhaseEntry != null)//__there are existing entries for this Project & Phase
-            {
-                //__update existing update count and use this for sequence number
-                int iOldSequenceNumber = Convert.ToInt32(projectPhaseEntry.UpdateCount);
-                iNewSequenceNumber = iOldSequenceNumber + 1;
-                projectPhaseEntry.UpdateCount = iNewSequenceNumber;
-                projectPhaseEntry.LatestUpdate = currentDT;
-            }
-            else //__since none was found we need a new entry
-            {
-                context.ProjectPhases.Add(new ProjectPhase()
-                {
-                    ProjectID = newUpdateID,
-                    PhaseID = iUpdatePhaseID,
-                    UpdateCount = 0,
-                    LatestUpdate = currentDT
-                });
-            }
-
-            foreach (StatusUpdate u in NewUpdates)
-            {
-                u.RecordDate = currentDT;
-                u.ProjectID = newUpdateID;
-                u.PhaseID = iUpdatePhaseID;
-                u.VerticalID = iVerticalID;
-
-                u.StatusSequence = iNewSequenceNumber;
-                context.StatusUpdates.Add(u);
-            }
-            context.SaveChanges();
-        }
-
-        public bool IsUserAuthorized(string email)
-        {
-            return context.AllowedUsers.Any(u => u.Email == email);
-        }
-
-        /// <summary>
-        /// Checks for the user in the database. If found RoleID (0,1,2) is returned
-        /// If user is not found, -1 is returned
-        /// </summary>
-        /// <param name="email"></param>
-        /// <returns></returns>
-        public int GetUserRole(string email)
-        {
-            int result = -1;
-            AllowedUser user = context.AllowedUsers.FirstOrDefault(u => u.Email == email);
-
-            if (user != null)
-            {
-                result = user.RoleID;
-            }
-            return result;
-        }
-
+        #region Authentication Methods
         public bool AddUser(string email, int userRole)
         {
             if (context.AllowedUsers.Any(a => a.Email == email)) return false;
@@ -155,6 +69,30 @@ namespace DataService
             return true;
         }
 
+        public bool IsUserAuthorized(string email)
+        {
+            return context.AllowedUsers.Any(u => u.Email == email);
+        }
+
+        /// <summary>
+        /// Checks for the user in the database. If found RoleID (0,1,2) is returned
+        /// If user is not found, -1 is returned
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>int indicating user role, or -1</returns>
+        public int GetUserRole(string email)
+        {
+            int result = -1;
+            AllowedUser user = context.AllowedUsers.FirstOrDefault(u => u.Email == email);
+
+            if (user != null)
+            {
+                result = user.RoleID;
+            }
+            return result;
+        }
+
+
         public bool UpdateUserRole(string email, int newUserRole)
         {
             if (context.AllowedUsers.Any(a => a.Email == email)) return false;
@@ -162,7 +100,7 @@ namespace DataService
             {
 
                 AllowedUser user = context.AllowedUsers.FirstOrDefault(u => u.Email == email);
-                user.RoleID = newUserRole;                
+                user.RoleID = newUserRole;
                 context.SaveChanges();
             }
             catch
@@ -199,12 +137,84 @@ namespace DataService
             context.SaveChanges();
             return true;
         }
+        #endregion
+
+
+        #region StatusUpdate and Project Methods
+
+        public bool? RecordStatusUpdate(List<StatusUpdate> updates)
+        {
+            //__safety check, cannot record an empty list
+            if (updates.Count == 0) return null;
+            try
+            {
+
+                DateTime currentDT = DateTime.Now;
+                string newUpdateID = updates[0].ProjectID;
+
+                //__check to see if Project exists, add it if not
+                Project existingProjectEntry = context.Projects.FirstOrDefault(p => p.ProjectID == newUpdateID);
+                if (existingProjectEntry == null) //_if there is no existing entry then add one
+                {
+                    context.Projects.Add(new Project()
+                    {
+                        ProjectID = newUpdateID,
+                        VerticalID = updates[0].VerticalID
+                    });
+                }
+
+                int iUpdatePhaseID = updates[0].PhaseID;
+                int iNewSequenceNumber = 0;
+               
+                ProjectPhase projectPhaseEntry = context.ProjectPhases.FirstOrDefault(p => p.ProjectID == newUpdateID && p.PhaseID == iUpdatePhaseID);
+
+                // check for existing entries for this Project & Phase
+                if (projectPhaseEntry != null)//__an entry exists
+                {
+                    //__update existing update count and use this for sequence number
+                    int iOldSequenceNumber = Convert.ToInt32(projectPhaseEntry.UpdateCount);
+                    iNewSequenceNumber = iOldSequenceNumber + 1;
+                    projectPhaseEntry.UpdateCount = iNewSequenceNumber;
+                    projectPhaseEntry.LatestUpdate = currentDT;
+                }
+                else //__since none was found we need a new entry
+                {
+                    context.ProjectPhases.Add(new ProjectPhase()
+                    {
+                        ProjectID = newUpdateID,
+                        PhaseID = iUpdatePhaseID,
+                        UpdateCount = 0,
+                        LatestUpdate = currentDT
+                    });
+                }
+
+                foreach (StatusUpdate u in updates)
+                {
+                    u.StatusSequence = iNewSequenceNumber;
+                    context.StatusUpdates.Add(u);
+                }
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        private List<StatusUpdate> GetAllUpdatesForProjectPhase(string projectID, int phaseID)
+        {
+            List<StatusUpdate> updates = new List<StatusUpdate>();
+            updates = context.StatusUpdates.Where(p => p.ProjectID == projectID && p.PhaseID == phaseID).ToList();
+            return updates;
+        }
 
         public List<Project> GetAllProjectNames()
         {
             return context.Projects.AsEnumerable().ToList();
 
-            
+
         }
 
         public List<StatusUpdate> GetAllUpdatesForProject(string projectID)
@@ -219,6 +229,6 @@ namespace DataService
 
         }
 
-       
+        #endregion
     }
 }
