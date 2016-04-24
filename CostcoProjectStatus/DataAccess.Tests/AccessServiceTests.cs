9eee27db1ca7674a8875294d0f12e57c27a3ad63
changl2 <lauratchang@gmail.com>
2016-04-24 11:29:41 -0700

    First unit case test

--- CostcoProjectStatus/DataAccess.Tests/AccessServiceTests.cs
+++ CostcoProjectStatus/DataAccess.Tests/AccessServiceTests.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using DataService;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatusUpdatesModel;
 
 namespace DataService.Tests
 {
     [TestClass()]
     public class AccessServiceTests
     {
+        int VERTICALENUM = 8;
         [TestMethod()]
         public void IsUserAuthorizedTest()
         {
@@ -18,7 +20,7 @@ namespace DataService.Tests
         public void GetUserRoleTest()
         {//__this covers AddUser, GetUserRole, DeleteUser, UpdateUserRole
             string emailAddy = "Test@mail.com";
-            string emailAddy2 = "Test2@mail.com";
+            //string emailAddy2 = "Test2@mail.com";
             int userRole = 1;
             AccessService dataAccess = new AccessService();
             try//__make sure we can back out in case of errors
@@ -48,30 +50,7 @@ namespace DataService.Tests
             }
         }
 
-        //[TestMethod]
-        //public void AddSUemails()
-        //{//__this covers AddUser, GetUserRole, DeleteUser, UpdateUserRole
-        //    List<string> emailAddys = new List<string>();
-        //    emailAddys.Add("seattleuni12@gmail.com");
-
-        //    int userRole = 0;
-        //    AccessService dataAccess = new AccessService();
-        //    try//__make sure we can back out in case of errors
-        //    {
-
-        //        foreach (var email in emailAddys)
-        //        {
-        //            dataAccess.AddUser(email, userRole);
-        //        }
-
-        //    }
-        //    catch (Exception e)
-        //    {//__make sure to clean out our test data
-
-        //    }
-        //}
-
-        [TestMethod]
+        [TestMethod()]
         public void AccessServiceConstructorWorks()
         {
             try
@@ -84,6 +63,68 @@ namespace DataService.Tests
                 Assert.Fail("AccessService constructor threw exception: " + e.Message);
 
             }
+        }
+
+        /// <summary>
+        /// Tests public accessable GetAllProjectsForVertical function. Checks to make sure that all valid vertical ID's returns
+        /// some form of valid data (brute force), and non valid verticals returns no data via boundary and random test.
+        /// </summary>
+        [TestMethod()]
+        public void GetAllProjectsForVerticalTest()
+        {
+            try
+            {
+                var dataAccess = new AccessService();
+                for (int verticalIter = 0; verticalIter < VERTICALENUM; verticalIter++)
+                {
+                    List<Project> allProjectsList = dataAccess.GetAllProjectsForVertical(verticalIter);
+                    Assert.AreNotEqual(allProjectsList, null);
+                }
+
+                // Boundary test #1: Vertical ID -1
+                checkForGetAllProjectsForVerticalFailure(-1);
+
+                // Boundary test #2: Vertical ID -2
+                checkForGetAllProjectsForVerticalFailure(-2);
+
+                // Boundary test #3: Vertical ID 8
+                checkForGetAllProjectsForVerticalFailure(8);
+
+                // Boundary test #4: Vertical ID 9
+                checkForGetAllProjectsForVerticalFailure(9);
+
+                // Random number test
+                Random random = new Random();
+                int randomNumber = random.Next(8, int.MaxValue);
+                checkForGetAllProjectsForVerticalFailure(randomNumber);
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("GetAppProjectsForVerticalAsync failed with this exception: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Helper function for GetAllProjectsForVerticalTest - whatever number that gets passed in should
+        /// not be between 0-8
+        /// </summary>
+        /// <param name="illVertNum"> Fake vertical ID which the access layer should return null</param>
+        private void checkForGetAllProjectsForVerticalFailure(int illVertNum)
+        {
+            try {
+                var dataAccess = new AccessService();
+                List<Project> illegalVertical = dataAccess.GetAllProjectsForVertical(illVertNum);
+                if ((illegalVertical != null && illegalVertical.Count > 0) && (illVertNum > 7 || illVertNum < 0 ))
+                {
+                    Assert.Fail("The vertical ID " + illVertNum + "exists and it should not!");
+                }
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("checkForGetAllProjectsForVerticalFailure in GetAppProjectsForVerticalAsync failed with this exception: " + e.Message);
+            }
+
         }
     }
 }
