ee8428fee458e2741bf4a49fa9eff85e54c3920a
Hasnath123 <umulh@seattleu.edu>
2016-04-26 19:28:16 -0700

    AddedCSV files to track

---
+++ CostcoProjectStatus/Read CSV/Read CSV/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Read_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService.AccessService dataService = new DataService.AccessService();
+            var file = new StreamReader(File.OpenRead(@"C:\test.csv"));
+            while (!file.EndOfStream)
+            {
+                var EachLine = file.ReadLine();
+                var valuesOfLine = EachLine.Split(',');
+                string userEmail = valuesOfLine[0];
+                int role = int.Parse(valuesOfLine[1]);
+                string Action = valuesOfLine[2];
+                if(Action == "add")
+                {
+                    dataService.AddUser(userEmail, role);
+                }
+                else
+                if (Action == "delete")
+                {
+                    dataService.DeleteUser(userEmail);
+                }
+                else
+                if (Action == "updateRole")
+                {
+                    dataService.UpdateUserRole(userEmail, role);
+                }
+                    // give old and new email
+                    //dataService.UpdateUserEmail(userEmail, user);        
+            }
+        }
+    }
+}
+
